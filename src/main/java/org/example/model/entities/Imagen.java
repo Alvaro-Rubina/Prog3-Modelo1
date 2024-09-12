b477d7957d37526587c7a4699457067955b7b506
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-12 20:07:34 -0300

    Creada clase Imagen

---
+++ src/main/java/org/example/model/entities/Imagen.java
@@ -0,0 +1,16 @@
+package org.example.model.entities;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Builder
+@ToString
+public class Imagen {
+
+    //
+    private Long id;
+    private String denominacion;
+
+}
