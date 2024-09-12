22af3de237b77a6ee80e6cd9e3034edad443e0a1
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-12 20:25:56 -0300

    Creacion de clase Main
    Se creó la clase Main. Dentro de esta, se crearon los repositorios con los que se trabajarán y se crearon instancias de Categoria.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,47 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.model.entities.*;
+import org.example.repository.InMemoryRepository;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.HashSet;
+
+public class Main {
+
+    // Repositorios
+    public static InMemoryRepository<ArticuloInsumo> articuloInsumoRepo = new InMemoryRepository<>();
+    public static InMemoryRepository<ArticuloManufacturado> articuloManuRepo = new InMemoryRepository<>();
+    public static InMemoryRepository<ArticuloManufacturadoDetalle> articuloManuDetalleRepo = new InMemoryRepository<>();
+    public static InMemoryRepository<Categoria> categoriaRepo = new InMemoryRepository<>();
+    public static InMemoryRepository<Imagen> imagenRepo = new InMemoryRepository<>();
+    public static InMemoryRepository<UnidadMedida> unidadMedidaRepo = new InMemoryRepository<>();
+
+    public static void main(String[] args) {
+
+        // Creacion de categoria Pizza
+        Categoria pizzasCat = Categoria.builder()
+                .denominacion("Pizzas")
+                .articulos(new HashSet<>())
+                .build();
+
+        Categoria sandwichesCat = Categoria.builder()
+                .denominacion("Sandwiches")
+                .articulos(new HashSet<>())
+                .build();
+
+        Categoria lomosCat = Categoria.builder()
+                .denominacion("Lomos")
+                .articulos(new HashSet<>())
+                .build();
+
+        Categoria insumosCat = Categoria.builder()
+                .denominacion("Insumos")
+                .articulos(new HashSet<>())
+                .build();
+
+        categoriaRepo.save(pizzasCat);
+        categoriaRepo.save(sandwichesCat);
+        categoriaRepo.save(lomosCat);
+        categoriaRepo.save(insumosCat);
+        
     }
 }
